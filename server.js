dd910dbd3c80ee5db24b58aa3023e66f1ab856ea
Zakaria12e <zakariael1212@gmail.com>
2025-03-27 02:44:27 +0000

    ,

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
