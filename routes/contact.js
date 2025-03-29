fd15ae219188cfa5d62d8edddb5ffcc543e08d46
Zakaria12e <zakariael1212@gmail.com>
2025-03-29 23:35:03 +0000

    add delete router

--- routes/contact.js
+++ routes/contact.js
@@ -23,7 +23,23 @@ router.get("/", async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: "Failed to fetch messages." })
     }
-  })
+  });
+
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedMessage = await Message.findByIdAndDelete(id);
+  
+      if (!deletedMessage) {
+        return res.status(404).json({ error: "Message not found" });
+      }
+  
+      res.status(200).json({ message: "Message deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to delete message" });
+    }
+  });
   
 
 module.exports = router;
